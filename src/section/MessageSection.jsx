5c9abdb07d3eea55f1552f04fac45646f21a85e0
sahal2255 <sahalvv9656@gmail.com>
2025-08-14 22:05:33 +0530

    message section completed

--- src/section/MessageSection.jsx
+++ src/section/MessageSection.jsx
@@ -1,34 +1,72 @@
 import { useGSAP } from "@gsap/react";
 import { SplitText } from "gsap/all";
+import gsap from "gsap";
 import React from "react";
 
 const MessageSection = () => {
+  useGSAP(() => {
+    const firstMsgSplit = SplitText.create(".msg-part-1", {
+      type: "words",
+    });
+    const secondMsgSplit = SplitText.create(".msg-part-2", {
+      type: "words",
+    });
+    const thirdMsgSplit = SplitText.create(".msg-part-3", {
+      type: "words",
+    });
+    let thirdMsgTl=gsap.timeline()
 
-    useGSAP(()=>{
-        const firstMsgSplit=SplitText.create(".msg-part-1",{
-            type:"words",
-        });
-        const secondMsgSplit=SplitText.create(".msg-part-1",{
-            type:"words",
-        });
-        const thirdMsgSplit=SplitText.create(".msg-part-1",{
-            type:"words,lines",
-            linesClass:""
-        })
-
+    gsap.to(firstMsgSplit.words, {
+      color: "#FFD300",
+      ease: "power1.in",
+      stagger: 0.5,
+      scrollTrigger: {
+        trigger: ".message-content",
+        start: "top center",
+        scrub: true,
+        end: "30% center",
+        markers: true,
+      },
+    });
+    gsap.to(secondMsgSplit.words, {
+      color: "#E21B4D",
+      textShadow: "0px 0px 15px #E21B4D", // glowing red
+      ease: "power1.in",
+      stagger: 0.5,
+      scrollTrigger: {
+        trigger: ".msg-part-2",
+        start: "top center",
+        scrub: true,
+        end: "30% center",
+        // markers: true
+      },
+    });
+    thirdMsgTl.from(thirdMsgSplit.words,{
+      xPercent:(i,target)=>(i%2===0 ? -100:100),
+      opacity:0,
+      stagger:0.1,
+      ease:"power2.out",
+      color:"#FFD300",
+      scrollTrigger:{
+        trigger:'.msg-part-3',
+        start:"top 80%",
+        end:"bottom 60%",
+        scrub:true
+      }
+,
     })
+  });
   return (
     <section className="message-content bg-main-bg">
       <div className="container mx-auto flex justify-center items-center py-28 relative">
         <div className="w-full h-full flex flex-col gap-8 text-center">
-          
           {/* Part 1 */}
-          <div className="msg-part-1 text-yellow-400 text-3xl md:text-5xl font-bold uppercase tracking-wide">
+          <div className="msg-part-1  text-3xl md:text-5xl font-bold uppercase tracking-wide">
             <h1>Fuel your fire.</h1>
           </div>
 
           {/* Part 2 */}
-          <div className="msg-part-2 text-red-500 text-3xl md:text-5xl font-bold italic">
+          <div className="msg-part-2 text-3xl md:text-5xl font-bold italic">
             <h1>Chase the thrill.</h1>
           </div>
 
@@ -36,7 +74,6 @@ const MessageSection = () => {
           <div className="msg-part-3 text-white text-3xl md:text-5xl font-extrabold drop-shadow-lg">
             <h1>Red Bull gives you wings.</h1>
           </div>
-
         </div>
       </div>
     </section>
