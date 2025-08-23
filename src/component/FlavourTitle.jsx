a21146724a4e7996e46d7ebef6d97b02371d0f3a
sahal2255 <sahalvv9656@gmail.com>
2025-08-23 22:13:26 +0530

    flavour section

--- src/component/FlavourTitle.jsx
+++ src/component/FlavourTitle.jsx
@@ -2,11 +2,26 @@ import React from 'react'
 
 const FlavourTitle = () => {
   return (
-    <div className="space-y-6">
-      <h2 className="text-4xl md:text-5xl font-bold ">
-       Ignite the Rush. Taste the Power. Own the Moment.
-      </h2>
-      
+    <div className="space-y-6 general-title col-center h-full 2xl:gap-32 xl:gap-24 text-center">
+      <div className="overflow-hidden py-3">
+        <h1 className="sporty-text text-4xl md:text-5xl font-extrabold uppercase tracking-tight">
+          WE HAVE 6
+        </h1>
+      </div>
+
+      <div className="relative inline-block">
+        <div
+          className="bg-[#8b4513] px-4 py-2 inline-block text-white font-black text-4xl md:text-5xl uppercase tracking-tight skew-x-[-10deg]"
+        >
+          FREAKING
+        </div>
+      </div>
+
+      <div>
+        <h1 className="sporty-text text-4xl md:text-5xl font-extrabold uppercase tracking-tight">
+          DELICIOUS FLAVORS.
+        </h1>
+      </div>
     </div>
   )
 }
