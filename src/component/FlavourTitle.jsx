07876a015793a35fa9c766cc08f3061abca6c9ed
sahal2255 <sahalvv9656@gmail.com>
2025-08-24 22:32:17 +0530

    flavour section partially completed

--- src/component/FlavourTitle.jsx
+++ src/component/FlavourTitle.jsx
@@ -1,33 +1,40 @@
-import { useGSAP } from "@gsap/react"
-import gsap from "gsap"
-import { SplitText,ScrollTrigger } from "gsap/all"
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { SplitText, ScrollTrigger } from "gsap/all";
 
-gsap.registerPlugin(ScrollTrigger)
+gsap.registerPlugin(ScrollTrigger);
 
 const FlavourTitle = () => {
-
-  useGSAP(()=>{
-    const tl=gsap.timeline({
-      delay:1,
+  useGSAP(() => {
+    const tl = gsap.timeline({
+      delay: 1,
       scrollTrigger: {
-        trigger: ".flavour-section",   // section wrapper
-        start: "top 50%",              // when top of section hits 80% viewport
-        toggleActions: "play none none none", 
-      once:true     }
+        trigger: ".flavour-section", 
+        start: "top 50%", 
+        toggleActions: "play none none reverse",
+        markers:true
+      },
     });
-    tl.to(".flavour-highlited",{
-      opacity:1,
-      duration:1,
-      clipPath:"polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
-      ease:"power2.inOut"
-    })
-    .to(".flavour-title",{
-      duration:1,
-      ease:"circ.out"
-    },
-    "-=0.5"
-  )
-  })
+    tl.to(".flavour-highlited", {
+      opacity: 1,
+      duration: 0.8,
+      clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+      ease: "power2.inOut",
+    }).to(
+      ".flavour-title",
+      {
+        duration: 1,
+        ease: "circ.out",
+      },
+      "-=0.3"
+    )
+    .fromTo(
+      ".flavour-text",   
+      { opacity: 0, scale: 0.8, y: 100 },
+      { opacity: 1, scale: 1, y: 0, duration: 1, ease: "back.out(1.7)" },
+      "-=0.3"
+    );
+  });
   return (
     <div className=" space-y-6 general-title col-center h-full 2xl:gap-32 xl:gap-24 text-center">
       <div className="overflow-hidden py-3">
@@ -36,13 +43,11 @@ const FlavourTitle = () => {
         </h1>
       </div>
 
-      <div className="relative inline-block flavour-highlited"
-        style={{ clipPath:"polygon(50% 0%, 50% 0, 50% 100%, 50% 100%)" }}
+      <div
+        className="relative inline-block flavour-highlited"
+        style={{ clipPath: "polygon(50% 0%, 50% 0, 50% 100%, 50% 100%)" }}
       >
-        <div
-          className="bg-[#8b4513] px-4 py-2 inline-block text-white font-black text-4xl md:text-5xl uppercase tracking-tight skew-x-[-10deg] flavour-text"
-         
-        >
+        <div className="bg-[#8b4513] px-4 py-2 inline-block text-white font-black text-4xl md:text-5xl uppercase tracking-tight skew-x-[-10deg] flavour-text">
           Flavor of Power
         </div>
       </div>
@@ -53,7 +58,7 @@ const FlavourTitle = () => {
         </h1>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default FlavourTitle
+export default FlavourTitle;
